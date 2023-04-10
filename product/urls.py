25d26739a625b184e07663bdba58d5175d8d75c5
pidokige02 <pidokige0204@gmail.com>
2023-04-10 20:25:58 +0900

    Add search functionality

--- product/urls.py
+++ product/urls.py
@@ -4,5 +4,7 @@ from product import views  # directory 에서 file import 하는 것이 가능�
 
 urlpatterns = [
     path('latest-products/', views.LatestProductsList.as_view()),
+    path('products/search/', views.search),
     path('products/<slug:category_slug>/<slug:product_slug>/', views.ProductDetail.as_view()),
+    path('products/<slug:category_slug>/', views.CategoryDetail.as_view()),
 ]
